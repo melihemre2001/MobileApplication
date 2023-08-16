e35cd81091842d381e7839a99bf129af7b56606e
melihemre2001 <melihemre2001@hotmail.com>
2023-08-16 11:18:55 +0300

    login screen has finished

--- app/src/main/java/com/mobile/application/MainActivity.kt
+++ app/src/main/java/com/mobile/application/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.mobile.application
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun handleKeyEvent(view: View,keyCode: Int): Boolean
+    {
+        if (keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 }
